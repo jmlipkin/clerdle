2093245d0a8ac81f27471dcdc634b24536b3b360
Jared Lipkin <jaredlipkin@gmail.com>
2024-05-13 13:29:30 -0400

    Just getting set up

---
+++ clerdle.cpp
@@ -0,0 +1,16 @@
+// ////////////////////////////////////////////////////////
+//
+// Author: Jared Lipkin
+//
+// Command-line port of game Nerdle. My solution to an
+// old final project assignment for an introductory
+// university C/C++ course.
+//
+// ////////////////////////////////////////////////////////
+
+int main(int argc, char* argv[])
+{
+    // Just setting up!
+
+    return 0;
+}
